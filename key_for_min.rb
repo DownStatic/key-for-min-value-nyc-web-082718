6d8aa9c739af0b0cd29a1ee207346fb8de0775b4
Christopher Stull <chris.stull@gmail.com>
2018-08-15 21:26:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   keys = []
   values = []
   answer_index = 0.5
-  name_hash.each do |key,value|
+  name_hash.collect do |key,value|
     keys << key
     values << value
   end
